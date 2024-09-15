578c0cc9b25d68b5ea5008547ef87f4e6e101fc1
Tajudeen Busari <53045507+TajudeenBusari@users.noreply.github.com>
2024-09-15 21:26:57 +0300

    This is the first commit.

---
+++ src/main/java/com/tjtechy/tjtechyinventorymanagementsept2024/TjtechyInventoryManagementSept2024Application.java
@@ -0,0 +1,13 @@
+package com.tjtechy.tjtechyinventorymanagementsept2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjtechyInventoryManagementSept2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TjtechyInventoryManagementSept2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tjtechy/tjtechyinventorymanagementsept2024/TjtechyInventoryManagementSept2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjtechy.tjtechyinventorymanagementsept2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjtechyInventoryManagementSept2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
