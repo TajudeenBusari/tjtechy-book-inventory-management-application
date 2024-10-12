ae4c34199ec5112d205640e26b3467e5f3898862
Tajudeen Busari <53045507+TajudeenBusari@users.noreply.github.com>
2024-10-12 18:28:48 +0300

    update the exception handler advice class so that we handle the insufficientAuthenticationException

--- src/main/java/com/tjtechy/tjtechyinventorymanagementsept2024/exceptions/ExceptionHandlerAdvice.java
+++ src/main/java/com/tjtechy/tjtechyinventorymanagementsept2024/exceptions/ExceptionHandlerAdvice.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.validation.FieldError;
@@ -64,6 +65,12 @@ public class ExceptionHandlerAdvice {
     @ResponseStatus(HttpStatus.UNAUTHORIZED)//normally, it should be unauthenticated but this is not available
     Result handleAuthenticationException(Exception ex) {
         return new Result(false, StatusCode.UNAUTHORIZED, "username or password is incorrect.", ex.getMessage());
+    }
+
+    @ExceptionHandler(InsufficientAuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    Result handleInsufficientAuthenticationException(InsufficientAuthenticationException ex) {
+        return new Result(false, StatusCode.UNAUTHORIZED, "Login credentials are missing", ex.getMessage());
     }
 
     @ExceptionHandler(AccountStatusException.class)
